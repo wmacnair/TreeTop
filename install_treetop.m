5b6243393bfb2589ffe77aeb2365a71289ae34b1
Macnair  Will <macnairw@ethz.ch>
2019-01-29 12:34:30 +0100

    tweaked compilation testing code after testing on linux

--- install_treetop.m
+++ install_treetop.m
@@ -31,7 +31,7 @@ end
 function check_compiled_functions()
 	% check this function
 	dijk_mex 	= check_mex_function('dijkstra.cpp', './TreeTop/private');
-	mi_mex 		= check_mex_function('MIToolboxMex.c', './MIToolbox/matlab');
+	mi_mex 		= check_mi_toolbox('MIToolboxMex.c', './MIToolbox/matlab');
 
 	% check each function works
 	dijk_works 	= false;
@@ -47,14 +47,14 @@ function check_compiled_functions()
 	if dijk_works & mi_works
 		fprintf('All compiled files available and working; TreeTop should run without any problems');
 	elseif dijk_works & ~mi_works
-		fprintf('The relevant compiled file for the mutual information toolbox was either not available or not working.')
-		fprintf('treetop should run ok, but outputs from the optional treetop_pre_run script will be incomplete.')
-		fprintf('To fix this, make sure you have a valid compiler installed and')
-		fprintf('compile the function ./MIToolbox/matlab/MIToolboxMex.');
+		fprintf('The relevant compiled file for the mutual information toolbox was either not available or not working.\n')
+		fprintf('treetop should run ok, but outputs from the optional treetop_pre_run script will be incomplete.\n')
+		fprintf('To fix this, make sure you have a valid compiler installed and\n')
+		fprintf('compile the function ./MIToolbox/matlab/MIToolboxMex.\n');
 	elseif ~dijk_works
-		fprintf('The relevant compiled file for the function dijkstra was either not available or not working.')
-		fprintf('treetop needs this to run; to fix this, make sure you have a valid compiler installed and')
-		fprintf('compile the function ./TreeTop/private/dijkstra.');
+		fprintf('The relevant compiled file for the function dijkstra was either not available or not working.\n')
+		fprintf('treetop needs this to run; to fix this, make sure you have a valid compiler installed and\n')
+		fprintf('compile the function ./TreeTop/private/dijkstra.\n');
 	end
 end
 
@@ -103,6 +103,51 @@ function mex_exists = check_mex_function(fn_name, fn_dir)
 	cd(start_dir)
 end
 
+function mex_exists = check_mi_toolbox(fn_name, fn_dir)
+	start_dir 	= cd;
+	cd(fn_dir)
+	fprintf('testing whether function %s is compiled\n', fn_name)
+
+	% Check input filename
+	assert(ischar(fn_name),'source_file must be a string')
+
+	% Check extension is specified
+	assert(~isempty(strfind(fn_name,'.')),'source_file: no file extension specified')
+
+	% Locate source file
+	[pathstr, name, ext] 	= fileparts(which(fn_name));
+	% Create filename, mex filename
+	filename 	= [pathstr filesep name ext];
+	mexfilename = [pathstr filesep name '.' mexext];
+
+	% check whether source exists
+    mex_exists  = false;
+	if strcmp(pathstr,'')
+		% source file not found
+		fprintf([source_file ': not found'])
+
+	% now check whether mexfile exists
+	elseif exist(mexfilename, 'file') ~= 3
+		 % if source file does not exist, try to compile
+		disp(['Compiling "' name ext '".'])
+
+		% compile, with options if appropriate
+		try
+			CompileMIToolbox
+			fprintf('Function %s successfully compiled\n', fn_name);
+			mex_exists 	= true;
+
+		catch lasterr
+			fprintf('Could not compile function %s. \n', fn_name);
+        end		
+    else
+        mex_exists  = true;
+    end
+
+	% switch back to original directory
+	cd(start_dir)
+end
+
 %% check_dijkstra_works: 
 function [dijk_works] = check_dijkstra_works()
 	dijk_works 	= false;
@@ -113,8 +158,8 @@ function [dijk_works] = check_dijkstra_works()
 	for ii = 1:n_nodes-1
 		G(ii, ii+1) 	= 1;
 		G(ii+1, ii) 	= 1;
-    end
-    G           = sparse(G);
+	end
+	G 			= sparse(G);
 	D_true 		= zeros(n_nodes);
 	for ii = 1:n_nodes
 		D_true(ii, :) 	= abs((1:n_nodes) - ii);
@@ -125,7 +170,7 @@ function [dijk_works] = check_dijkstra_works()
 	cd('./TreeTop/private')
 	try
 		D 			= dijkstra(G, (1:n_nodes)');
-		dijk_works 	= isequal(D, D_true);
+		dijk_works 	= all(all(abs(D - D_true) < 1e-14));
 	catch lasterr
 		% fprintf('dijkstra did not work\n')
 	end
